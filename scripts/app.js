0e264dc6894c584613cbdb0b01d5626f5193f982
yiwanaguchi <yiwanaguchi@necp.co.jp>
2020-05-28 15:23:59 +0900

    Added load cookie function

--- scripts/app.js
+++ scripts/app.js
@@ -3,8 +3,13 @@ var save_coockie_btn = document.getElementById('save_cookie_btn');
 if (save_coockie_btn) {
     save_coockie_btn.addEventListener('click', (event) => {
         save_cookie();
+        load_cookie();
     });
 }
+
+var loaded_cookie_label = document.getElementById('loaded_cookie');
+
+var cookie_name1 = "cookie1";
 
 function save_cookie() {
     let date = new Date();
@@ -13,7 +18,15 @@ function save_cookie() {
 
     let value = document.getElementById('input_value').value;
     //coknam+"="+escape(coktxt)+";expires="+endday;
-    let cookie = "cookie1=" +escape(value) + ";expire=" + end_date;
+    let cookie = cookie_name1 + "=" +escape(value) + ";expire=" + end_date;
     document.cookie = cookie;
     console.log("[save_cookie] wrote ", cookie);
+}
+
+function load_cookie() {
+    let cookies = document.cookie;
+    let target = cookies.split(";").find(cookie => cookie.indexOf(cookie_name1) >= 0)[0];
+    let value = target.split("=")[0];
+    
+    loaded_cookie_label.innerText = value;
 }
