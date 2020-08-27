e4fb32e8a178ce1317fdd4d9d8cb54780f96d232
yiwanaguchi <yiwanaguchi@necp.co.jp>
2020-08-27 09:55:10 +0900

    Changed

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 const CACHE_NAME = 'static-cache-v1';
 const FILES_TO_CACHE = [
     '/',
-    '/index.html',
+    //'/index.html',
     //'/scripts/app.js',
     ///'/scripts/install.js',
     '/styles/font-awesome.min.css',
