ba83fb4a1fc549fd54b7d1a43ecba4b7bfaa06d3
yiwanaguchi <yiwanaguchi@necp.co.jp>
2020-09-01 10:55:02 +0900

    Changed

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-var revision = 10117;
+var revision = 10118;
 
 function set_revision() {
     document.getElementById('update_stamp').innerText = revision;
@@ -80,7 +80,6 @@ window.addEventListener('beforeunload', () => {
         connection.close();
     }
 });
-
 
 // Opening handshake
 var connect_server = document.getElementById('connect_server');
@@ -214,100 +213,13 @@ function start_handshake() {
 
 }
 
-
-
-
-
 var ws_close_pwa = document.getElementById('ws_close_pwa');
 ws_close_pwa.addEventListener('click', (event) => {
-    connection.send("ibact=16&mode=0&id=0569u7srjsy6");
+    connection.send("id=" + identifier + "&as=16");
 });
 
-var ws_close_pwa_and_open_url= document.getElementById('ws_close_pwa_and_open_url');
-ws_close_pwa_and_open_url.addEventListener('click', (event) => {
-    var encoded_url = encodeURIComponent(document.getElementById('open_url').value);
-    connection.send("ibact=18&mode=0&id=0569u7srjsy6&url=" + encoded_url);
-});
-
-
-var ws_get_key = document.getElementById('ws_get_key');
-ws_get_key.addEventListener('click', (event) => {
-    connection.send("ibact=1&mode=0&id=0569u7srjsy6");
-});
-
-
-var post_publish = document.getElementById('post_publish');
-var dummy = document.getElementById('dummy');
-post_publish.addEventListener('click', (event)=> {
-
-    var req = new XMLHttpRequest();
-    req.onreadystatechange = function() {
-        if (req.readyState == 4) {
-            if (req.status == 200) {
-                dummy.innerText = req.responseText;
-                ticket = req.responseText;
-            }
-        }
-    };
-
-    req.open('get', 'http://localhost:8090/ws/ticket/identifier', true);
-    req.setRequestHeader("X-Ib-Fetch", "accept");
-    req.send(null);
-
-    /*
-    fetch('http://localhost:8090/ws/ticket/identifier', {
-        method: 'GET',
-        headers: {
-            'X-Ib-Fetch': "accept",
-        }
-    })
-    .then((response) => {
-        if (!response.ok) {
-            throw new Error(`${response.status} ${response.statusText}`);
-        }
-        return response.text();
-    })
-    .then((text) => {
-        ticket = text;
-        dummy.innerText = text;
-    })
-    .catch(error => console.error(error));
-    */
-});
-
-// Sned ticket test
-var ws_ticket_test = document.getElementById('ws_ticket_test');
-ws_ticket_test.addEventListener('click', () => {
-    // for testing ticket validation
-    var escaped_ticket = encodeURIComponent(ticket);
-    var url = "ws://localhost:8090/ws/hs/identifier/" + escaped_ticket;
-    connection = new WebSocket(url);
-
-    var connection_result = document.getElementById('connection_result');
-    connection.onopen = function(event) {
-        var result_text = event.data === undefined ? "" : " (" + event.data + ")"
-        connection_result.innerText = "onopen" + result_text;
-        connection_message.innerText = "";
-    };
-
-    connection.onerror = function(event) {
-        var result_text = event.data === undefined ? "" : " (" + event.data + ")"
-        connection_result.innerText = "onerror" + result_text;
-        connection_message.innerText = "";
-    };
-
-    connection.onmessage = function(event) {
-        connection_result.innerText = "onmessage"
-        connection_message.innerText = event.data;
-    };
-
-    connection.onclose = function() {
-        connection_result.innerText = "onclose";
-        connection_message.innerText = "";
-    };
-
-});
-
+////////////////////////////////////////////////////////////////////////////////
+// Encryption
 // Get public key for encryption
 var get_key = document.getElementById('get_key');
 var public_key_value = document.getElementById('public_key_value');
