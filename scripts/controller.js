6148ca6cc16ef7f42e6d9f8fec92bc94921c9c2b
yiwanaguchi <yiwanaguchi@necp.co.jp>
2020-10-14 15:16:22 +0900

    Added

---
+++ scripts/controller.js
@@ -0,0 +1,17 @@
+const IbPwaController = class {
+    constructor() {
+
+    }
+
+    event = {
+        // Controller -> Ads
+        all: 1000,
+        prepareAds: 1001,
+
+        // Ads -> Controller
+    };
+
+    test() {
+        return "This is test";
+    }
+};
---
+++ scripts/test.js
@@ -0,0 +1,4 @@
+import IbPwaController as Controller from './scripts/IbPwaController';
+
+let result = Controller.test();
+let item2 = Controller.event.prepareAds;
