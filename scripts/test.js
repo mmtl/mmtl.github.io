7be52d2abe2b0d80a94203170cd3b506e35f8bb0
yiwanaguchi <yiwanaguchi@necp.co.jp>
2020-10-14 15:42:46 +0900

    Changed

--- scripts/test.js
+++ scripts/test.js
@@ -1,4 +1,4 @@
-import IbPwaController as Controller from './IbPwaController.js';
+import * as Controller from './IbPwaController.js';
 
 let result = Controller.test();
 let item2 = Controller.event.prepareAds;
