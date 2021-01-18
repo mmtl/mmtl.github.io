fd95116491818d93739bb43bdc2d517ba0f8f871
yiwanaguchi <yiwanaguchi@necp.co.jp>
2021-01-18 10:01:55 +0900

    Add

---
+++ test/ce.js
@@ -0,0 +1,14 @@
+class TestElement extends HTMLElement {
+    constructor() {
+        super();
+
+        this.shadow = this.attachShadow({mode: 'open'});
+        const element = document.createElement("h1");
+        element.textContent = TestA.getContent();
+        this.shadow.append(element);
+    }
+}
+
+(() => {
+    customElements.define("test-ele", TestElement);
+})();
