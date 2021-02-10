e79fd604ab686fb6564fa2e4d825ede8dde13828
yiwanaguchi <yiwanaguchi@necp.co.jp>
2021-02-10 17:41:24 +0900

    Add

---
+++ scripts/IbPwaConst.js
@@ -0,0 +1,29 @@
+export const IbPwaConst = class {
+    static backgrounds = [
+        {
+            name: "InfoWindow.Resource.Images.IBBG.aflo_AXHA018774.webp",
+            copyright: "河口湖 富士山 ©AFLO, Hideaki Tanaka",
+            order: 0
+        },
+        {
+            name: "InfoWindow.Resource.Images.IBBG.aflo_MOXA008394.webp",
+            copyright: "Test2 ©AFLO, Hideaki Tanaka",
+            order: 0
+        },
+        {
+            name: "InfoWindow.Resource.Images.IBBG.aflo_RYBA002526.webp",
+            copyright: "Test3 ©AFLO, Hideaki Tanaka",
+            order: 0
+        },
+        {
+            name: "InfoWindow.Resource.Images.IBBG.aflo_RYBA008459.webp",
+            copyright: "Test4 ©AFLO, Hideaki Tanaka",
+            order: 0
+        },
+        {
+            name: "InfoWindow.Resource.Images.IBBG.aflo_WUYB010076.webp",
+            copyright: "Test5 ©AFLO, Hideaki Tanaka",
+            order: 0
+        },
+    ];
+};
